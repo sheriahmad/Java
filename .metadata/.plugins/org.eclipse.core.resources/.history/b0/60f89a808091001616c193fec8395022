4e42d1d16dbbf0922333fd0b5b6ab208b1799178
“sheriahmad” <sheriahmad@hotmail.com>
2016-11-03 21:10:55 -0700

    Tweet Arcostics

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/b0/60f89a808091001616c193fec8395022
@@ -0,0 +1,9 @@
+
+public class tests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ HackerRankSolutions/Scope/src/tests.java
@@ -0,0 +1,15 @@
+
+public class tests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 11; i <= 15; i++) {
+
+			System.out.println(i);
+			System.out.println(i*1000);
+		}
+
+	}
+
+}
