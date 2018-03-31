43cb195d0624d86e265a221474531ca792771a2a
Sheheryar Ahmad <sheriahmad@hotmail.com>
2018-03-31 11:02:17 -0700

    PracticeIt
    Commiting all the college assignments once and for all.

---
+++ PracticeIt/src/PracticeItPrintNumbers.java
@@ -0,0 +1,23 @@
+/**
+ * Write a method called printNumbers that accepts a maximum number as a parameter and prints each number from 1 up to that maximum, inclusive, boxed by square brackets. For example, consider the following calls:
+ * printNumbers(15);
+ * printNumbers(5);
+ * These calls should produce the following output:
+ * [1] [2] [3] [4] [5] [6] [7] [8] [9] [10] [11] [12] [13] [14] [15]
+ * [1] [2] [3] [4] [5]You may assume that the value passed to printNumbers is 1 or greater.
+ * 
+ * @author Sheheryar Ahmad
+ */
+public class PracticeItPrintNumbers {
+	public static void main(String[] args) {
+		PrintNumbers(15);
+		PrintNumbers(5);
+	}
+
+	private static void PrintNumbers(int size) {
+		// TODO Auto-generated method stub
+		for(int i = 1; i <= size; i++) {
+			System.out.printf("[%d] ", i);
+		}	
+	}
+}
